251e8c3c368be26b75ebca75e226c2802c6584ae
Cristi Olteanu <cristiolteanu1892@gmail.com>
2024-09-01 13:43:24 +0300

    selectionSort

---
+++ sorting_algorithms/selectionSort.py
@@ -0,0 +1,12 @@
+def selectionSort(arr):
+
+    for i in range(len(arr)-1):
+        min_idx = i
+        for j in range(i+1, len(arr)):
+            if arr[min_idx] > arr[j]:
+                min_idx = j
+        arr[i], arr[min_idx] = arr[min_idx], arr[i]
+    return arr
+
+
+print(selectionSort([64, 25, 12, 22, 11]))
