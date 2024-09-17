c009253f7ae6abcc6a5f2e97ac8178d8ea52473c
Cristi Olteanu <cristiolteanu1892@gmail.com>
2024-09-17 11:07:08 +0300

    Update 153_findMin.py

--- python_problems/153_findMin.py
+++ python_problems/153_findMin.py
@@ -50,7 +50,7 @@ def findMin(nums):
         mid = (l + r) // 2
         res = min(res, nums[mid])
 
-        if nums[l] < nums[mid]:
+        if nums[l] <= nums[mid]:
             l = mid + 1
         else:
             r = mid - 1
@@ -58,5 +58,5 @@ def findMin(nums):
 
 
 print(findMin([3,4,5,1,2]))
-#print(findMin([4,5,6,7,0,1,2]))
-#print(findMin([11,13,15,17]))
+print(findMin([2,1]))
+print(findMin([11,13,15,17]))
