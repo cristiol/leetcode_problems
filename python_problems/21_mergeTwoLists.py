1235b141adf5305a00f273c11f76ce1b59e0a30e
Cristi Olteanu <cristiolteanu1892@gmail.com>
2024-09-28 10:57:42 +0300

    Create 21_mergeTwoLists.py

---
+++ python_problems/21_mergeTwoLists.py
@@ -0,0 +1,96 @@
+"""
+You are given the heads of two sorted linked lists list1 and list2.
+
+Merge the two lists into one sorted list. The list should be made by splicing together the nodes of the first two lists.
+
+Return the head of the merged linked list.
+
+
+
+Example 1:
+
+
+Input: list1 = [1,2,4], list2 = [1,3,4]
+Output: [1,1,2,3,4,4]
+Example 2:
+
+Input: list1 = [], list2 = []
+Output: []
+Example 3:
+
+Input: list1 = [], list2 = [0]
+Output: [0]
+
+
+Constraints:
+
+The number of nodes in both lists is in the range [0, 50].
+-100 <= Node.val <= 100
+Both list1 and list2 are sorted in non-decreasing order.
+"""
+from pickle import dumps
+
+
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+    def __str__(self):
+        return f"{str(self.val)} -> {str(self.next)}"
+
+
+class Solution:
+    def mergeTwoLists(self, l1, l2):
+        dummy = ListNode()
+        tail = dummy
+        print(dummy, tail)
+
+        while l1 and l2:
+            print('l1', l1, 'l2', l2)
+            if l1.val < l2.val:
+                tail.next = l1
+                l1 = l1.next
+            else:
+                tail.next = l2
+                l2 = l2.next
+            tail = tail.next
+
+
+        if l1:
+            tail.next = l1
+        elif l2:
+            tail.next = l2
+        return dummy.next
+
+
+
+# Helper function to convert a list to a linked list
+def list_to_linkedlist(lst):
+    if not lst:
+        return None
+    head = ListNode(lst[0])
+    current = head
+    for value in lst[1:]:
+        current.next = ListNode(value)
+        current = current.next
+    return head
+
+
+# Helper function to convert a linked list to a list
+def linkedlist_to_list(node):
+    result = []
+    while node:
+        result.append(node.val)
+        node = node.next
+    return result
+
+
+solution = Solution()
+
+l1 = list_to_linkedlist([1,2,4])
+l2 = list_to_linkedlist([1,3,4])
+reversed_list = solution.mergeTwoLists(l1,l2)
+print(linkedlist_to_list(reversed_list))
+
