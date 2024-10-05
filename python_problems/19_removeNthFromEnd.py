b66ef9ad085fa4856a5c19fa977e16eccc7a9b11
Cristi Olteanu <cristiolteanu1892@gmail.com>
2024-10-05 13:19:15 +0300

    Create 19_removeNthFromEnd.py

---
+++ python_problems/19_removeNthFromEnd.py
@@ -0,0 +1,87 @@
+"""
+Medium
+Topics
+Companies
+You are given the head of a singly linked-list. The list can be represented as:
+
+L0 → L1 → … → Ln - 1 → Ln
+Reorder the list to be on the following form:
+
+L0 → Ln → L1 → Ln - 1 → L2 → Ln - 2 → …
+You may not modify the values in the list's nodes. Only nodes themselves may be changed.
+
+
+
+Example 1:
+
+
+Input: head = [1,2,3,4]
+Output: [1,4,2,3]
+Example 2:
+
+
+Input: head = [1,2,3,4,5]
+Output: [1,5,2,4,3]
+
+
+Constraints:
+
+The number of nodes in the list is in the range [1, 5 * 104].
+1 <= Node.val <= 1000
+"""
+
+
+
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+    def __str__(self):
+        return f"{str(self.val)} -> {str(self.next)}"
+
+
+class Solution:
+    def removeNthFromEnd(self, head, n):
+        dummy = ListNode(0, head)
+        left = dummy
+        right = head
+
+        while n > 0 and right:
+            right = right.next
+            n -= 1
+
+        while right:
+            left = left.next
+            right = right.next
+
+        left.next = left.next.next
+        return dummy.next
+
+
+# Helper function to convert a list to a linked list
+def list_to_linkedlist(lst):
+    if not lst:
+        return None
+    head = ListNode(lst[0])
+    current = head
+    for value in lst[1:]:
+        current.next = ListNode(value)
+        current = current.next
+    return head
+
+
+# Helper function to convert a linked list to a list
+def linkedlist_to_list(node):
+    result = []
+    while node:
+        result.append(node.val)
+        node = node.next
+    return result
+
+
+solution = Solution()
+head = list_to_linkedlist([1,2,3,4,5])
+solution.removeNthFromEnd(head,2)
+print(linkedlist_to_list(head))
